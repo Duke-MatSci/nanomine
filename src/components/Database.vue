c94ad4a4c8119da42e3665535218637d1d5fc076
NanoMineVM <bingyin.hu@gmail.com>
2018-07-10 12:05:10 -0400

    three columns display, sequence changed

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -1,51 +1,48 @@
 <template>
   <div class="database">
     <h1>{{ msg }}</h1>
-    <v-container>
-      <div>
-        <!--//{% if user|has_perm:'curate.curate_access' %}-->
-        <img src="/cdn/img/excel.png" />
-        <h4>
-          <router-link to="/XMLCONV">Curate Using Uploader</router-link>
-        </h4>
-        <p>
-          Download an Excel template, enter nanocomposite sample or simulation data into the template and upload. This is the simplest way.
-        </p>
-        <!--//{% endif %}-->
-
-        <!--//{% if user|has_perm:'explore.explore_access' %}-->
-        <img src="/cdn/img/edit-big.png" />
-        <h4>
-          <router-link to="/curate">Curate online</router-link>
-        </h4>
-        <p>
-          Select a schema and fill out the corresponding form. Using the Excel template upload is usually simpler.
-        </p>
-        <!--//{% endif %}-->
-      </div>
-
-      <div>
-        <!--//{% if user|has_perm:'compose.compose_access' %}-->
-        <img src="/cdn/img/search-big.png" />
-        <h4>
-          <router-link to="/explore">Explore the repository</router-link>
-        </h4>
-        <p>Search this nanocomposite data resource using flexible queries.</p>
-        <!--//{% endif %}-->
-
-        <!--//{% if user|has_perm:'curate.curate_access' %}-->
-        <img src="/cdn/img/edit-big.png"/>
-        <h4>
-          <router-link to="/compose">Compose a schema</router-link>
-        </h4>
-        <p>Compose your own schema.</p>
-        <!--//{% endif %}-->
-      </div>
-      <img src="/cdn/img/visualize_data.png"/>
-      <h4>
-        <router-link to="/visualize">Visualize</router-link>
-      </h4>
-      <p>Visualize nanocomposite data.</p>
+    <v-container grid-list-xl>
+      <v-layout row wrap>
+        <v-flex xs4>
+          <img src="/cdn/img/visualize_data.png"/>
+          <h4>
+            <router-link to="/visualize">Visualize</router-link>
+          </h4>
+          <p>Visualize nanocomposite data.</p>
+        </v-flex>
+        <v-flex xs4>
+          <img src="/cdn/img/excel.png" />
+          <h4>
+            <router-link to="/XMLCONV">Curate Using Uploader</router-link>
+          </h4>
+          <p>
+            Download an Excel template, enter nanocomposite sample or simulation data into the template and upload. This is the simplest way.
+          </p>
+        </v-flex>
+        <v-flex xs4>
+          <img src="/cdn/img/edit-big.png" />
+          <h4>
+            <router-link to="/curate">Curate online</router-link>
+          </h4>
+          <p>
+            Select a schema and fill out the corresponding form. Using the Excel template upload is usually simpler.
+          </p>
+        </v-flex>
+        <v-flex xs4>
+          <img src="/cdn/img/search-big.png" />
+          <h4>
+            <router-link to="/explore">Explore the repository</router-link>
+          </h4>
+          <p>Search this nanocomposite data resource using flexible queries.</p>
+        </v-flex>
+        <v-flex xs4>
+          <img src="/cdn/img/edit-big.png"/>
+          <h4>
+            <router-link to="/compose">Compose a schema</router-link>
+          </h4>
+          <p>Compose your own schema.</p>
+        </v-flex>
+      <v-layout>
     </v-container>
   </div>
 </template>
