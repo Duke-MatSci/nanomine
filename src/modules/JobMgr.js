e1236931fe25ee66df233e99302684cb59914ea9
bluedevil-oit <ed151@duke.edu>
2018-08-24 16:11:30 -0400

    updates for job submission

--- src/modules/JobMgr.js
+++ src/modules/JobMgr.js
@@ -17,12 +17,24 @@ JobMgr.prototype = {
     return this.jobId
   },
   handleErr: function (err, failureFunction) {
-    if (err.response) {
-      failureFunction(err.response.status, err.response.statusText)
-    } else if (err.request) {
-      failureFunction(500, err)
+    let vm = this
+    if (err.config) {
+      console.log(err.config)
     }
-    console.log(err.config)
+    if (err.response) {
+      if (err.config && err.config.data) {
+        let data = JSON.parse(err.config.data)
+        if (data.jobFileInfo) {
+          vm.jobInputFiles[data.jobFileInfo.idx].statusCode = err.response.status
+          vm.jobInputFiles[data.jobFileInfo.idx].statusText = err.response.statusText
+        }
+      }
+      return failureFunction(err.response.status, err.response.statusText)
+    } else if (err.request) {
+      return failureFunction(500, err)
+    } else {
+      return failureFunction(500, err)
+    }
   },
   submitJob: function (successFunction, failureFunction) {
     // this.jobId = SET by remote call
@@ -31,19 +43,58 @@ JobMgr.prototype = {
       setTimeout(failureFunction(400, 'Job type required'), 0) // don't do this on main path -- it's supposed to be async
     } else {
       // create job to get jobId and initialize job directory
-      Axios.post(vm.createJobPath, {
-        'jobParameters': vm.jobParameters,
-        'jobType': vm.jobType
-      })
-        .then(function (res) {
-          vm.jobId = res.data.data.jobId
-          // still need to send files
-          // still need to send parameters and submit job
-          successFunction(vm.jobId)
+      let fileSends = []
+      try {
+        Axios.post(vm.createJobPath, {
+          'jobParameters': vm.jobParameters,
+          'jobType': vm.jobType
         })
-        .catch(function (err) {
-          vm.handleErr(err, failureFunction)
-        })
+          .then(function (res) {
+            vm.jobId = res.data.data.jobId
+            vm.jobInputFiles.forEach(function (v) {
+              // send each file in a separate request and wait for all to complete successfully before submitting job
+              fileSends.push(Axios.post(vm.postJobFilePath, {
+                'jobType': vm.jobType,
+                'jobFileInfo': v
+              }))
+            })
+            Axios.all(fileSends)
+              .then((p) => {
+                console.log(p)
+                // wait for all files to be sent then submit job
+                p.forEach(function (v, idx) {
+                  // console.log('logging response info below: ')
+                  // console.log(v)
+                  // set status and status text vm.jobInputFiles[idx].statusCode=p.
+                  let reqData = JSON.parse(v.config.data)
+                  let index = reqData.jobFileInfo.idx
+                  vm.jobInputFiles[index].statusCode = v.status
+                  vm.jobInputFiles[index].statusText = v.statusText
+                })
+                Axios.post(vm.submitJobPath, {
+                  'jobId': vm.jobId,
+                  'jobType': vm.jobType
+                })
+                  .then(function (res) {
+                    console.log('submit job success - statusCode: ' + res.status + ' statusText: ' + res.statusText)
+                    return successFunction(vm.jobId)
+                  })
+                  .catch(function (err) {
+                    console.log('submit job failure' + err)
+                    vm.handleErr(err, failureFunction)
+                  })
+              })
+              .catch(function (err) {
+                console.log('Axios.all catch' + err)
+                vm.handleErr(err, failureFunction)
+              })
+          })
+          .catch(function (err) {
+            vm.handleErr(err, failureFunction)
+          })
+      } catch (ex) {
+        vm.handleErr(ex, failureFunction)
+      }
     }
   },
   setJobType: function (jobType) {
@@ -52,8 +103,19 @@ JobMgr.prototype = {
   getJobType: function () {
     return this.jobType
   },
-  addInputFile: function (fileName, dataUrl) {
-    this.jobInputFiles.push({'fileName': fileName, 'dataUrl': dataUrl})
+  getFileCount: function () {
+    return this.jobInputFiles.length
+  },
+  getFileInfo: function (idx) {
+    let rv = null
+    if (idx >= 0 && idx < this.jobInputFiles.length) {
+      rv = this.jobInputFiles[idx]
+    }
+    return rv
+  },
+  addInputFile: function (fileName, dataUri) {
+    let idx = this.jobInputFiles.length
+    this.jobInputFiles.push({ 'idx': idx, 'fileName': fileName, 'dataUri': dataUri, 'statusCode': null, 'statusText': null })
   },
   setJobParameters: function (paramsObject) {
     this.jobParameters = paramsObject
@@ -73,6 +135,10 @@ JobMgr.prototype = {
       console.log('Success')
     }, function failure (err) {
       console.log(err)
+      for( let i=0; i < jm.getFileCount(); ++i) {
+        let fileInfo = jm.getFileInfo(i)
+        console.log('file name: ' + fileInfo.fileName + ' statusCode: ' + fileInfo.statusCode + ' statusText: ' + fileInfo.statusText)
+      }
   })
 
 */
