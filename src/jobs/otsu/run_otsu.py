cd811ba52ce469942de52ca3f15c544ac7f641df
bluedevil-oit <ed151@duke.edu>
2018-09-19 18:23:20 -0400

    added otsu email templates and job

---
+++ src/jobs/otsu/run_otsu.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# run the example Otsu process
+
+# Environment variables will be set, so they're available here.
+
+import os
+import sys
+import json
+import urllib2
+
+import logging # this should be done by a wrapper in the future
+sloglevel=os.environ['NM_LOGLEVEL']
+loglevel = logging.DEBUG
+try:
+  loglevel = getattr(logging, sloglevel.upper())
+except:
+  pass
+
+logging.basicConfig(filename=os.environ['NM_LOGFILE'], level=loglevel)
+
+
+logging.info('nanomine environment keys: ')
+for key in os.environ.keys():
+  if key.startswith('NM_'):
+    logging.info('  ' + key)
+
+logging.info('--------')
+
+jobBaseDir = os.environ['NM_JOB_DATA']
+pgmName = sys.argv[0]
+jobType = sys.argv[1]
+jobId = sys.argv[2]
+jobDir = sys.argv[3]
+
+paramFile = open(jobDir + '/' + 'job_parameters.json', 'r')
+inputParameters = json.load(paramFile)
+
+logging.info(pgmName + ' input parameters: ')
+for key in inputParameters.keys():
+  logging.info( '  ' + key + ' = ' + inputParameters[key])
+
+# Do not modify job_status.json -- the server will handle it
+logging.info(pgmName + ' input files: ')
+myfiles = os.listdir(jobDir)
+for f in myfiles:
+  print('  ' + f)
+
+
+
+# example sending error template (rough cut)
+# for now the email just goes into the rest server log file: nanomine/rest/nanomine.log.gz (log file name will get fixed soon)
+# templates are in rest/config/emailtemplates/JOBTYPE/TEMPLATENAME.etf (etf extension is required, but implied in POST data)
+try:
+  emailurl = 'http://localhost/nmr/jobemail'
+  logging.info('emailurl: ' + emailurl)
+  emaildata = {
+    "jobid": jobId,
+    "jobtype": jobType,
+    "emailtemplatename": "failure",
+    "emailvars": {
+      "jobinfo": {
+        "resultcode":99
+      },
+      "user": str(inputParameters['user'])
+    }
+  }
+  print('email data: %s' % emaildata)
+  #logging.info('emaildata: ' + json.dumps(emaildata))
+  rq = urllib2.Request(emailurl)
+  logging.info('request created using emailurl')
+  rq.add_header('Content-Type','application/json')
+  r = urllib2.urlopen(rq, json.dumps(emaildata))
+  logging.info('sent failure email: ' + str(r.getcode()))
+except:
+  logging.info('exception occurred')
+  logging.info('exception: ' + str(sys.exc_info()[0]))
+
+
+
