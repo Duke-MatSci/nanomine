08e7f6ca2047123d865e679d46f5f3da783da925
bluedevil-oit <ed151@duke.edu>
2019-02-19 16:17:45 -0500

    updated installer

--- install/install.sh
+++ install/install.sh
@@ -13,10 +13,10 @@ fi
 if [[ -z $(which pip) ]]; then
   echo 'Installing pip - python package installer'
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  sudo python get-pip.py
+  (su root -c 'python get-pip.py')
 else
   echo 'Already installed: pip -- updating pip'
-  sudo pip install --upgrade pip
+  (su root -c 'pip install --upgrade pip')
 fi
 
 echo installing curl and git
