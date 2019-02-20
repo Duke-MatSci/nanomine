2199569026e6aaa059ac2cbe9c2cc39e4be5b063
bluedevil-oit <ed151@duke.edu>
2019-02-20 17:07:18 -0500

    installer updates

--- install/setup_mongo.sh
+++ install/setup_mongo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source /apps/nanomine_env
+echo "$0 environment $(env)"
 /apps/nanomine/install/mongoSetupAdminUser
 /apps/nanomine/install/mongoSetupApiUser
 /apps/nanomine/install/mongoSetupOwnerUser
