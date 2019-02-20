4d74f9e52cff9d47f83fc38eb3e4537e9e4cc3be
bluedevil-oit <ed151@duke.edu>
2019-02-20 11:09:03 -0500

    installer update

--- install/install.sh
+++ install/install.sh
@@ -75,7 +75,7 @@ export dttm=$(date +%Y%m%d%H%M%S)
 grep nanomine_env /etc/init.d/celeryd 2>/dev/null
 if [[ $? -gt 0 ]] ; then
   cp /etc/init.d/celeryd /etc/init.d/celeryd.backup-${dttm}
-  export tmpFile = $(mktemp)
+  export tmpFile=$(mktemp)
   $(sed -e 's/\. \/lib\/lsb\/init-functions/\. \/apps\/nanomine_env\n\. \/lib\/lsb\/init-functions/' /etc/init.d/celeryd > ${tmpFile})
   cp ${tmpFile} /etc/init.d/celeryd
   // rm ${tmpFile}
