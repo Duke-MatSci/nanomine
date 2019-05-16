87389cadd63eceb84ddf27671eaa66480eb566f8
bluedevil-oit <ed151@duke.edu>
2019-05-16 08:38:50 -0400

    responsive layout work

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,10 +1,9 @@
 <template>
-  <v-footer app class="footer" v-if="show()">
+  <v-footer app v-bind:class="footerfont" v-if="show()">
     <v-flex align-center>
     &copy; 2014-{{ new Date().getFullYear() }} NanoMine
     &nbsp;|&nbsp; <a href="https://oarc.duke.edu/privacy/duke-university-privacy-statement" target="_blank">Privacy Policy</a>
     &nbsp;|&nbsp; <a href="https://security.duke.edu/node/82" target="_blank">Terms of Use</a>
-    &nbsp;|&nbsp; <router-link to="/contact">Contact</router-link>
     </v-flex>
   </v-footer>
 </template>
@@ -18,6 +17,15 @@ export default {
   data () {
     return {
       msg: 'PageFooter'
+    }
+  },
+  computed: {
+    footerfont () {
+      console.log('breakpoint info: ' + JSON.stringify(this.$vuetify.breakpoint))
+      return {
+        'footer-font-small': this.$vuetify.breakpoint.xsOnly, // smAndDown
+        'footer-font': this.$vuetify.breakpoint.smAndUp
+      }
     }
   },
   methods: {
@@ -39,8 +47,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.footer { // not used
+.footer {
   background-color: #505050;
+}
+.footer-font-small {
+  font-size: 12px;
+}
+.footer-font {
+  font-size: 20px;
 }
 a {
   color: #505050;
