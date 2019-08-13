5e78e718f9485a7ce9f708070ab089163fdaa787
bluedevil-oit <ed151@duke.edu>
2019-08-13 09:07:55 -0400

    small installer updates

--- rest/update_api_tokens.sh
+++ rest/update_api_tokens.sh
@@ -3,6 +3,7 @@ export REST_DIR=/apps/nanomine/rest
 
 export tmpFile=$(mktemp)
 echo $tmpFile
+# remove the old tokens
 sed -e 's/export NM_AUTH_SYSTEM_TOKEN.*//' /apps/nanomine_env |
 sed -e 's/export NM_AUTH_API_TOKEN_CURATE.*//'   |
 sed -e 's/export NM_AUTH_API_REFRESH_CURATE.*//' |
@@ -14,6 +15,7 @@ sed -e 's/export NM_AUTH_API_TOKEN_ADMIN.*//'    |
 sed -e 's/export NM_AUTH_API_REFRESH_ADMIN.*//'  |
 sed -r '/^\s*$/d' > $tmpFile  # GNU sed extension to remove blank lines
 
+# create new tokens
 cp $tmpFile /apps/nanomine_env
 echo 'export NM_AUTH_SYSTEM_TOKEN="'$(/apps/nanomine/install/random.js)'"' >> /apps/nanomine_env
 echo 'export NM_AUTH_API_TOKEN_CURATE="'$(node ${REST_DIR}/updateRestApiToken.js updateToken curate)'"' >> /apps/nanomine_env
