0bdef82be45cd85529847048c476c14391750092
bluedevil-oit <ed151@duke.edu>
2019-08-15 10:09:25 -0400

    reenable bash_profile update

--- install/retrieve_mongo_dump.sh
+++ install/retrieve_mongo_dump.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 if [[ $(whoami) != "whyis" ]]; then
   echo 'please run ${0} script as whyis.  Mongo Dump not retrieved.'
   exit
 fi
+
 mkdir /apps/mongodump
 if [[ $NM_MONGO_DUMP == "file://*" ]]; then
   cp_err_msg="Unable to copy ${fn} mongo dump originally referenced as ${NM_MONGO_DUMP}"
@@ -15,6 +17,8 @@ if [[ $NM_MONGO_DUMP == "file://*" ]]; then
   else
     echo $cp_err_msg
   fi
+elif [[ -z $NM_MONGO_DUMP ]]; then
+  echo error '$NM_MONGO_DUMP' not defined
 else
   curl -k -o /apps/mongodump/mgi.tgz $NM_MONGO_DUMP
 fi
