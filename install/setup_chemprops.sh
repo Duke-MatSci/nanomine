86fda04bffb3406aa539206b8d36ddc786eb34e8
bluedevil-oit <ed151@duke.edu>
2019-08-30 14:50:45 -0400

    install chemprops gs config

--- install/setup_chemprops.sh
+++ install/setup_chemprops.sh
@@ -9,7 +9,8 @@ fi
 pip install -r /apps/chemprops/requirements.txt
 echo 'export NM_MONGO_CHEMPROPS_DB=chemprops' >> nanomine_env
 echo 'export NM_MONGO_CHEMPROPS_URI="mongodb://${NM_MONGO_API_USER}:${NM_MONGO_API_PWD}@${NM_MONGO_HOST}:${NM_MONGO_PORT}/${NM_MONGO_CHEMPROPS_DB}"'  >> nanomine_env
-
+echo 'export NM_AUTOSTART_CHEMPROPS="no"' >> nanomine_env
+wget -O /apps/chemprops/gs.config ${CHEMPROPS_GOOGLE_CONFIG_LOCATION}
 mongo --port $NM_MONGO_PORT -u $NM_MONGO_USER -p $NM_MONGO_PWD --authenticationDatabase admin <<EOF
 use chemprops
 db.createUser(
