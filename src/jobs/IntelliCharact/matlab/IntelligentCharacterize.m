3f884d1bdae191e3dcc2187e3b77a83767d5df1f
mail4umar <umarghumman2018@u.northwestern.edu>
2019-09-09 15:11:07 -0500

    updated teh intelligent characterization page

--- src/jobs/IntelliCharact/matlab/IntelligentCharacterize.m
+++ src/jobs/IntelliCharact/matlab/IntelligentCharacterize.m
@@ -12,7 +12,7 @@ try
     path_to_write = [jobSrcDir, '/output'];
     mkdir(path_to_write);
     writeError([path_to_write, '/errors.txt'], '');
-    
+
     %% Specify import function according to input option
     switch str2num(input_type)
         case 1
@@ -32,12 +32,12 @@ try
                 writeError([path_to_write, '/errors.txt'], msg);
                 writeError([path_to_write, '/errors.txt'], newline);
                 exit(rc);
-            end     
+            end
     end
-    
+
     % Otsu binarize
 
-    
+
     if str2double(input_type) ~= 2
         if length(size(img)) > 2
             img = img(:,:,1);
@@ -50,55 +50,69 @@ try
         end
         imwrite(256*img, [path_to_write, '/', 'Input1.jpg']);
     end
-    
+
     % Deal with odd shaped images
     md = min(size(img));
     img = img(1:md,1:md);
-    
-    
-    
-    
+
+
+
+
     %% Image read done, now work on output
-  
+
     SDFsize = 200;
-    
+
     vol_frac = mean(img(:));  % Volume fraction
     perim = bwperim(img);
     intf_area = sum(perim(:));  % Interfacial area
-    
+
     [connectivity, concavity] = check_shape(img);
     sdf2d = fftshift(abs(fft2(img-vol_frac)).^2);
     [isotropy, ~] = check_isotropy(sdf2d);
-    if all(connectivity == [1, 0]) && (concavity < 1.2)
+
+    csvtarget = [path_to_write, '/output.csv'];
+    fid = fopen(csvtarget,'w');
+    fprintf(fid, '%s%s%s\n', 'Sample name', ',', file_name);
+    fprintf(fid, '%s%s%s%s%s%s%s\n', 'Universal descriptors', ',', 'Void fraction', ',',...
+        'Interfacial area', ',', 'Isotropy');
+    fprintf(fid, '%s%s%f%s%f%s%f\n', ' ', ',', vol_frac, ',', intf_area, ',', isotropy);
+
+    if all(connectivity == [1, 0]) || all(connectivity == [0, 1]) && (concavity < 1.2)
         % Use descriptors
+        fprintf(fid, '%s%s%s\n', 'Characterization method selected', ',', 'Speroidal descriptors');
+        fprintf(fid, '\n');
+        fprintf(fid, '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n', 'Spheroidal descriptors', ',', ...
+            'Mean neighbor distance', ',', 'Cluster number', ',', 'Compactness', ',', 'Cluster area', ...
+            ',', 'Cluster radius', ',', 'Elongation ratio', ',', 'Orientation angle', ',', 'Rectangularity');
         cimg = bwlabel(img);
-        mcr_method = 'Spheroidal_descriptor';
-        [sph.mean_neighbor_distance, ~, ~] = nearest_center_distance(cimg);
-        [sph.cluster_number, compactness, c_area, c_radius] = faster_nh_ch(cimg);
+        [mean_neighbor_distance, ~, ~] = nearest_center_distance(cimg);
+        [cluster_number, compactness, c_area, c_radius] = faster_nh_ch(cimg);
         [elong_ratio, ~, orien_angle, rectangularity] = faster_elongation_II(cimg);
-        % Calculate mean and variance
-        sph.compactness = struct('mean', mean(compactness), 'variance', var(compactness));
-        sph.mean_cluster_area = struct('mean', mean(c_area), 'variance', var(c_area));
-        sph.mean_cluster_radius = struct('mean', mean(c_radius), 'variance', var(c_radius));
-        sph.elongation_ratio = struct('mean', mean(elong_ratio), 'variance', var(elong_ratio));
-        sph.orientation_angle = struct('mean', mean(orien_angle), 'variance', var(orien_angle));
-        sph.rectangularity = struct('mean', mean(rectangularity), 'variance', var(rectangularity));
-        sdf = NaN;
-        
+        fprintf(fid, '%s%s%f%s%f%s%f%s%f%s%f%s%f%s%f%s%f\n', 'Mean value', ',', mean_neighbor_distance, ',',...
+            cluster_number, ',', mean(compactness), ',', mean(c_area), ',', mean(c_radius), ',', mean(elong_ratio),...
+            ',', mean(orien_angle), ',', mean(rectangularity));
+        fprintf(fid, '%s%s%s%s%s%s%f%s%f%s%f%s%f%s%f%s%f\n', 'Variance', ',', ' ', ',', ' ', ...
+            ',', var(compactness), ',', var(c_area), ',', var(c_radius), ',', var(elong_ratio), ...
+            ',', var(orien_angle), ',', var(rectangularity));
+
+
     else  % Isotropy check is not included yet
         % Use SDF
-        mcr_method = 'SDF';
-        
+        fprintf(fid, '%s%s%s\n', 'Characterization method selected', ',', 'SDF');
+        fprintf(fid, '\n');
+        fprintf(fid, '%s\n', 'SDF');
+
         sdf1d = FFT2oneD(sdf2d);
         fit_result = SDFFit(sdf1d, SDFsize, true, path_to_write);  % true - show fitting image
-        sdf.function_type = fit_result('func_type');
-        sdf.parameters = fit_result('parameters');
-        sdf.function_definition = fit_result('func_def');
-        sdf.remarks = fit_result('remarks');
+        fprintf(fid, '%s%s%s\n', 'Fitting function', ',', fit_result('func_type'));
+        fprintf(fid, '%s', 'Fitting parameters');
+        fprintf(fid, ',%f', fit_result('parameters'));
+        fprintf(fid, '\n');
+        fprintf(fid, '%s%s%s%s%s\n', 'Fitting goodness', ',', 'R^2', ',', 'rmse');
         gof = fit_result('goodness');
-        sdf.goodness.rsquare = gof(1);
-        sdf.goodness.rmse = gof(2);
-        sph = NaN;
+        fprintf(fid, '%s%s%f%s%f\n', ' ', ',', gof(1), ',', gof(2));
+        fprintf(fid, '%s%s%s\n', 'Function definition', ',', fit_result('func_def'));
+        fprintf(fid, '%s%s%s\n', 'Remarks', ',', fit_result('remarks'));
         % Save 2D SDF image
         figure('color',[1,1,1])
         hold on;
@@ -111,28 +125,18 @@ try
         xlim([0 size(img,1)]); ylim([0 size(img,2)]);
         set(gca,'xtick',[]); set(gca,'ytick',[]);
         saveas(gcf,[path_to_write,'/SDF_2D.jpg']);
-        hold off 
+        hold off
     end
-    % Root of the storage tree
-    root.name = 'file_name';
-    root.volume_fraction = vol_frac;
-    root.interfacial_area = intf_area;
-    root.isotropy = isotropy;
-    root.characterization = mcr_method;
-    root.sdf = sdf;
-    root.spherical_descriptor = sph;
-    result = jsonencode(root);  % Output data in json
-    save([path_to_write, '/output.json'], 'result', '-ascii')
-    % save([sp_path, sp_name, '.json'], result)
-    
+
     % Zip files
+    fclose(fid);
     zip([path_to_write, '/Results.zip'], {'*'}, [path_to_write, '/']);
-    
+
 catch ex
     rc = 99;
     exit(rc);
 end
-    
+
     function writeError(file, msg)
     f = fopen(file, 'a+');
     fprintf(f, '%s\n', msg);
