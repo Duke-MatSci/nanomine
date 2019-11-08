17f25caa2b06ca970fb00fbf958f09a4d9e61d22
bluedevil-oit <ed151@duke.edu>
2019-11-08 08:02:48 -0500

    updated videolink component icon settings

--- src/store/index.js
+++ src/store/index.js
@@ -22,15 +22,15 @@ const store = new Vuex.Store({
     cms: { // CMS object contains references to urls by name that can be loaded. At some point, this will be dyn loaded.
       images: {},
       videos: {
-        'nanomine/mcr/intelligent_characterization_tutorial': {
-          url: 'https://materialsmine.org/nmf/Intelligent_Characterization_Tutorial_by_Umar.mp4',
-          title: 'Intelligent Characterization Tutorial',
-          text: 'This narrated video tutorial shows how to use the Intelligent Characterization tool and view results.'
-        },
         'nanomine/visualization_tutorial': {
           url: 'https://materialsmine.org/nmf/nanomine-vis.mp4',
           title: 'Visualization Tutorial',
           text: 'This video tutorial show basic usage of the search and visualization tool. (no audio)'
+        },
+        'nanomine/mcr/intelligent_characterization_tutorial': {
+          url: 'https://materialsmine.org/nmf/Intelligent_Characterization_Tutorial_by_Umar.mp4',
+          title: 'Intelligent Characterization Tutorial',
+          text: 'This narrated video tutorial shows how to use the Intelligent Characterization tool and view results.'
         },
         'nanomine/mcr/basic_module_tool_tutorial': {
           url: 'https://materialsmine.org/nmf/NanoMine_Demo_by_Akshay.mp4',
