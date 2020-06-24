bb5f5fbe16cb0bb6eba5f166bb06c8d5239e8cd0
Atul Jalan <atuljalan17@gmail.com>
2020-06-23 17:44:15 -0700

    lfdsaf

--- src/components/EditImage.vue
+++ src/components/EditImage.vue
@@ -15,7 +15,7 @@
     <div class='modal' v-if='value'>
         <div class='modal-container'>
             <h1>{{ title }}</h1>
-            <cropper class='cropper' imageClassname='cropper-image' :src='source_image' @change='onChange'></cropper>
+            <cropper :src='source_image' class='cropper' imageClassname='cropper-image' @change='onChange'></cropper>
             <div class='modal-container-buttons'>
                 <v-btn color="primary" v-on:click='closeModal()'>Cancel</v-btn>
                 <v-btn color="primary" v-on:click='saveImage()'>Save</v-btn>
@@ -89,7 +89,7 @@
         height: 700px;
         margin-top: 48px;
         max-width: 90%;
-        max-height: (90% - 48px);
+        max-height: calc(90% - 48px);
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
@@ -102,9 +102,9 @@
     .cropper {
         max-width: 80%;
     }
-    
+
     .cropper-image {
-        margin-top: 0px;
+        margin-top: 0px !important;
     }
 
     .modal-container-buttons {
