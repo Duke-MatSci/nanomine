532f98eb7048e9325d1dd9672f42c46c0a49ec70
Atul Jalan <atuljalan17@gmail.com>
2020-06-26 00:13:19 -0700

    cleaned up code

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -16,37 +16,29 @@
 
         <p class="text-xs-left">Select File
             <v-btn class="text-xs-left" small color="primary" @click='pickFile'>Browse</v-btn>
-            <input
-            type="file"
-            style="display: none"
-            accept=".jpg, .png, .tif, .mat, .zip"
-            ref="myUpload"
-            @change="onFilePicked"
-            >
+            <input type="file" style="display: none" accept=".jpg, .png, .tif, .mat, .zip" ref="myUpload" @change="onFilePicked">
         </p>
 
+        <!-- Image Cropper Modal -->
         <EditImage v-model='imageEditorOpen' v-bind:img='imageEditorData.fileUrl' v-bind:imgName='imageEditorData.fileName' v-on:setCroppedImage="setCroppedImage"></EditImage>
 
         <v-list v-model="fileName" subheader: true v-if="fileUploaded">
             <template v-for="(file, index) in filesDisplay">
-
-                <v-list-tile
-                :key="file.fileName"
-                >
+                <v-list-tile :key="file.fileName">
 
                     <v-list-tile-avatar>
                         <v-icon color="primary">check_circle_outline</v-icon>
                     </v-list-tile-avatar>
+
                     <v-list-tile-content>
                         <v-list-tile-title v-text="file.fileName"></v-list-tile-title>
                     </v-list-tile-content>
 
-                    <span v-if='fileName.split(".").pop() !== "mat"' :key='file.fileName'>
+                    <span v-if='fileType !== "mat"' :key='file.fileName'>
                         <v-btn :key='index' v-on:click="openImageEditor(index)" color="primary">Edit image</v-btn>
                     </span>
 
                 </v-list-tile>
-
             </template>
         </v-list>
 
@@ -61,20 +53,25 @@
 
     export default {
         name: 'ImageUpload',
+
         components: {
             EditImage
         },
+
         data() {
             return {
                 files: [],
                 filesDisplay: [],
+                fileType: '',
                 fileName: '',
                 fileUploaded: false,
                 imageEditorOpen: false,
                 imageEditorData: {fileUrl: null, fileName: null}
             }
         },
+
         methods: {
+
             openImageEditor: function (index) {
                 this.imageEditorData = this.filesDisplay[index]
                 this.imageEditorOpen = !this.imageEditorOpen // toggle the image editor modal being open and closed
@@ -83,12 +80,12 @@
             // args: [cropped image, filename of cropped image, coordinates]
             setCroppedImage: async function (...args) {    
                 for (let i = 0; i < this.filesDisplay.length; i++){
-                    if (this.filesDisplay[i].fileName == args[1]){
+                    if (this.filesDisplay[i].fileName === args[1]){
 
                         this.filesDisplay[i].fileUrl = args[0];
                         this.filesDisplay[i].fileName = "cropped_" + this.filesDisplay[i].fileName; // to ensure that the list of images reloads since they are binded to filenames.
                         
-                        if (this.files[0].fileName.split('.').pop() == 'zip'){
+                        if (this.fileType == 'zip'){
                             await this.cropAllImages(args[2], "cropped_" + args[1])
                             this.rezipFiles()
                         } else {
@@ -114,51 +111,52 @@
             },
 
             onFilePicked (e) {
-                this.resetFiles()
-                const files = e.target.files
-                for (let i = 0; i < files.length; i++) {
-                    let file = {}
-                    let f = files[i]
-                    if (f !== undefined) {
-                        file.fileName = f.name
-                        this.fileName = f.name
-                        if (file.fileName.lastIndexOf('.') <= 0) {
-                            return
-                        }
-                        console.log(file.fileName)
-                        const fr = new FileReader()
-                        fr.readAsDataURL(f)
-                        fr.addEventListener('load', () => {
-                            file.fileUrl = fr.result
-                            this.files.push(file)
-                            this.fileUploaded = true
 
-                            if (file.fileName.split('.').pop() != 'zip'){
-                                this.filesDisplay.push(file)
-                            }
-                        })
+                this.resetFiles();
+                const input_file = e.target.files[0];
+                let file = {};
 
-                    } else {
-                        console.log('File Undefined')
-                    }
+                // set filename
+                if (input_file !== undefined) {
+                    file.fileName = input_file.name;
+                    this.fileName = input_file.name;
                 }
+
+                // check for acceptable filetype
+                const accepted_types = ['jpg', 'jpeg', 'tif', 'png', 'mat', 'zip'];
+                const fileType = input_file.name;
+                this.fileType = fileType;
+                if (accepted_types.includes(fileType) === false) {
+                    return;
+                }
+
+                // extract data from uploaded file
+                const fr = new FileReader();
+                fr.readAsDataURL(input_file)
+                fr.addEventListener('load', () => {
+                    file.fileUrl = fr.result
+                    this.files.push(file)
+                    this.filesDisplay.push(file)
+                    this.filesUploaded = true
+                })
 
                 if (files[0].name.split('.').pop() == 'zip') {
                     this.unzipFiles(files[0])
                 }
 
-                this.$emit('setFiles', this.files, this.fileName)
+                this.$emit('setFiles', file, file.fileName)
+
             },
 
             unzipFiles (input_file) {
+
                 const vm = this;
                 vm.filesDisplay = [];
                 const jszip_obj = new jszip();
 
                 jszip_obj.loadAsync(input_file)
                 .then(async function(zip) {
-                    console.log('unzipped file')
-                    let count = 0;
+
                     for (var key in zip.files){
 
                         // get file data
@@ -177,8 +175,11 @@
 
                         // set to reactive variables
                         vm.filesDisplay.push({fileName: filename, fileUrl: base64});      
+
                     }
+
                     console.log('finished extracting images')
+
                 });
             },
 
@@ -201,9 +202,10 @@
                 })
             },
 
-            async cropAllImages (coordinates, fileName) {
+            cropAllImages (coordinates, fileName) {
+
                 for (let i = 0; i < this.filesDisplay.length; i++){
-                    if (this.filesDisplay[i].fileName != fileName){
+                    if (this.filesDisplay[i].fileName !== fileName){
                     
                         var canvas = document.createElement('canvas')
                         canvas.width = coordinates.width;
