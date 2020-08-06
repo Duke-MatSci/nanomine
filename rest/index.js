91ee19e2a0b70c41dd1fe4331de9f66cbc13e3bc
Atul Jalan <atuljalan17@gmail.com>
2020-08-05 19:31:41 -0700

    please

--- rest/index.js
+++ rest/index.js
@@ -3881,25 +3881,24 @@ mongoose
   .connect(
     dbUri, {useNewUrlParser: true, keepAlive: true, keepAliveInitialDelay: 300000, useUnifiedTopology: true, reconnectTries: 2, reconnectInterval: 500}
   ).then(result => {
-    const server = app.listen(3000);
-    const io = require('./rest-initializer/socket').init(server);
-    io.on('connection', socket => {
-      socket.on('disconnect', () => {
-        if (socket.sockets[socket.id]) {
-          socket.sockets[socket.id].disconnect();
-        }
-      })
-      // socket.on('newJob', jobId => {
-      //   socketConnections[jobId] = socket.id
-      // })
-    })
   }).catch(err => logger.error('db error: ' + err))
 
 // const emitResults = (jobId, data) => {
 //   io.to(currentJobs[jobId].emit('finished', data))
 // }
 
-
+const server = app.listen(3000);
+const io = require('./rest-initializer/socket').init(server);
+io.on('connection', socket => {
+  socket.on('disconnect', () => {
+    if (socket.sockets[socket.id]) {
+      socket.sockets[socket.id].disconnect();
+    }
+  })
+  // socket.on('newJob', jobId => {
+  //   socketConnections[jobId] = socket.id
+  // })
+})
 
 /*
 prefix dataset: <https://hbgd.tw.rpi.edu/dataset/>
