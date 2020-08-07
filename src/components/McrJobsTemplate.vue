b8deeaa5a30bd1654704f6fb60246dffc76380b5
Atul Jalan <atuljalan17@gmail.com>
2020-08-07 15:54:45 -0700

    updating path

--- src/components/McrJobsTemplate.vue
+++ src/components/McrJobsTemplate.vue
@@ -102,7 +102,7 @@ import {Auth} from '@/modules/Auth.js'
 import {JobMgr} from '@/modules/JobMgr.js'
 import {} from 'vuex'
 import io from 'socket.io-client'
-var socket = io('http://localhost/nmr')
+var socket = io({path: '/nmr/socket.io'})
 
 export default {
 
