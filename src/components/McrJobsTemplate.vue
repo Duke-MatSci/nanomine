af1dd521a762b8fa6a21f0fb1a98cca55262f88a
Atul Jalan <atuljalan17@gmail.com>
2020-08-10 18:59:35 -0700

    another try on socket

--- src/components/McrJobsTemplate.vue
+++ src/components/McrJobsTemplate.vue
@@ -102,7 +102,7 @@ import {Auth} from '@/modules/Auth.js'
 import {JobMgr} from '@/modules/JobMgr.js'
 import {} from 'vuex'
 import io from 'socket.io-client'
-var socket = io({path: '/nmr/socket.io', transports: ['websocket']})
+var socket = io({path: '/nmr/socket.io', secure: true, transports: ['websocket']})
 
 export default {
 
