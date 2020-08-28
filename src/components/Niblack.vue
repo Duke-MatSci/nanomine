eb9c92f723cdd796da8b44addeed74ff444390b6
Atul Jalan <atuljalan17@gmail.com>
2020-08-27 19:03:28 -0700

    small updates to niblack descriptions

--- src/components/Niblack.vue
+++ src/components/Niblack.vue
@@ -37,8 +37,8 @@ export default {
         pageTitle: 'Binarization - Niblack Method',
 
         description: [
-          'Niblack is a local thresholding method for binarization which involves computing for each pixel in the grayscale image the mean and standard devation of the colors of the neighboring pixels in an area (WINDOW SIZE) of predefined size.',
-          'WINDOW SIZE: Niblack involves computing for each pixel in the grayscale image the mean and standard devation of the colors of the neighboring pixels in an area (i.e. window size) of predefined size. Window size is generally between 1-50, depending on the size of the particles of interest.',
+          'Niblack is a local thresholding method for binarization that is particularly useful for images without uniform backgrounds and images where calculating a global threshold does not yield good results.',
+          'WINDOW SIZE: Niblack involves computing, for each pixel in the grayscale image, the mean and standard devation of the colors of the neighboring pixels in an area (i.e. window size) of predefined size. Window size is generally between 1-50, depending on the size of the particles of interest.',
           'K: an image dependent manually selected parameter (generally, the value is -0.2 for dark foreground and +0.2 for dark background).',
           'OFFSET: an offset value that is applied when computing the mean and standard deviation for threshold values for pixels in an image (to separate foreground and background pixels.) Most of the time, the offset value can be 0.',
           'Upload a grayscale micrograph image in .jpg/.png/.tif format and click "Binarize" to perform dynamic niblack binarization. A default window size of 5 pixels (i.e. 5 x 5 pixel window) will be used.',
@@ -77,15 +77,15 @@ export default {
 
         selects: [
           {
-            title: 'Window Size',
+            title: 'Window Size (usually between 1 and 50)',
             submitJobTitle: 'WindowSize',
           },
           {
-            title: 'K (Optional)',
+            title: 'K (Optional, usually between -0.2 and +0.2)',
             submitJobTitle: 'KNiblack'
           },
           {
-            title: 'Offset (Optional)',
+            title: 'Offset (Optional, usually set to 0)',
             submitJobTitle: 'OffsetNiblack'
           }
         ]
