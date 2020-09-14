259fc5702d91cc689f2041fd35dec1455cb45beb
tolulomo <tolulomofateye@gmail.com>
2020-09-14 19:10:01 -0400

    Chemmprops api and rest update

--- pyserver/app/util/auth_helper.py
+++ pyserver/app/util/auth_helper.py
@@ -7,15 +7,16 @@ class Auth:
   def get_jwt_token_info(request):
     status = 200
     token_info = {}
-    token = request.cookies.get('token')
+    # token = request.cookies.get('token')
+    token = request.headers.get('token')
     app = Config.getApp()
-    if not token:
-      app.logger.debug(__name__ + ' token is NOT available')
-      status = 401
-      return status, token_info
-    else:
+    if token:
       status_str, token_info = decode_jwt_token(token)
       app.logger.debug(__name__ + ' obtained token info: ' + str(token_info) + ' status: ' + status_str)
       if status_str != 'ok' : # could be anonymous, but they're still not logged in
         status = 401
+      return status, token_info
+    else:
+      app.logger.debug(__name__ + ' token is NOT available')
+      status = 401
       return status, token_info
--- rest/middlewares/tempVerifier.js
+++ rest/middlewares/tempVerifier.js
@@ -3,6 +3,10 @@
 ***/
 const { body, validationResult } = require('express-validator');
 const User = require('../modules/mongo/schema/users');
+const jwt = require('jsonwebtoken');
+const nanomineUtils = require('../modules/utils')
+const env = nanomineUtils.getEnv()
+const nmAuthSecret = env.nmAuthSecret
 
 exports.check = [
     body('creator')
@@ -45,4 +49,33 @@ exports.getUser = async(req, res, next) => {
         logger.error('NM-Rest Chart Gallery User Validation Error: Server error, cannot pull data from mongo')
         throw error;
     }
+}
+
+exports.verifyToken = async(req, res, next) => {
+    let error, decodedToken;
+    const logger = req.logger;
+    const authHeader = req.get('Authorization')
+    if(!authHeader){
+        error = new Error('Not Authenticated')
+        error.statusCode = 402
+        logger.error("User failed to pass authentication token!")
+        throw error
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        decodedToken = jwt.verify(token, nmAuthSecret)
+        if(!decodedToken) {
+            logger.error("Error! Token verification decoding return empty string")
+            error = new Error('Authentication failed')
+            error.statusCode = 401
+            throw error
+        }
+        req.userToken = decodedToken
+    } catch(err){
+        error = err
+        error.statusCode = 500
+        logger.error("Token verification failed!")
+        throw error
+    }
+    next();
 }
