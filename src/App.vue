8645df09ec63f0010e97ab6519c6fc7838847845
tolulomo <tolulomofateye@gmail.com>
2020-12-07 14:55:50 -0500

    update for new ux integration

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,70 @@
 <template>
-  <v-app id="app" app>
-    <waiting/>
-    <LeftMenu/>
-    <page-header/>
-    <page-subheader/>
-    <router-view class="app-router"/>
-    <page-footer v-if="$route.path === '/'" />
-  </v-app>
+  <div v-if="versionNew">
+    <router-view />
+  </div>
+  <div v-else>
+    <v-app id="app" app>
+      <waiting/>
+      <LeftMenu/>
+      <page-header/>
+      <page-subheader/>
+      <router-view class="app-router"/>
+      <page-footer v-if="$route.path === '/'" />
+    </v-app>
+  </div>
 </template>
-
 <script>
 
-import {} from 'vuex'
-
+// import {} from 'vuex'
+const newDev = ['/home_v2', '/teams'];
 export default {
   name: 'App',
-  data () {
-    return {}
+  computed: {
+    versionNew(){
+      return this.$store.state.versionNew;
+    }
   },
-  beforeMount: function () {
-    console.log('beforeMount of App.vue')
+  watch: {
+    '$route.path': function(newPath, oldPath){
+      this.checkVersion(newPath,oldPath, undefined)
+    }
   },
   methods: {
     showLeftMenu: function () {
       return this.$store.state.leftMenuActive
+    },
+    checkVersion (newPath,oldPath, path) {
+      if(path) {
+        if (newDev.includes(path)) {
+          return this.$store.state.versionNew = true;
+        }
+        return this.$store.state.versionNew = false;
+      } else if(newPath) {
+        if (newDev.includes(newPath)) {
+          return this.$store.state.versionNew = true;
+        }
+        return this.$store.state.versionNew = false;
+      } else if(newPath !== oldPath && !newPath) {
+        if (newDev.includes(oldPath)) {
+          return this.$store.state.versionNew = true;
+        }
+        return this.$store.state.versionNew = false;
+      }
     }
+  },
+  beforeMount(){
+    let path = this.$router.history.current.path
+    path = path.trim()
+    return this.checkVersion(undefined, undefined, path);
   }
 }
 </script>
 
 <style>
-@import './css/styles.css';
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 48px;
-    background-color: white;
-  }
-  .app-router {
-    // margin-bottom: 75px;
-    margin-top: -10px;
-  }
-  h1 {
+  @import './css/styles.css';
+  @import './css/old_css.css';
+
+  /** h1 {
     margin-top: 25px;
     margin-bottom: 15px;
   }
@@ -53,10 +73,22 @@ export default {
     margin-top: 25px;
   }
 
-  img {
-    margin-top: 30px;
+  .utility_old {
+    img {
+      margin-top: 30px;
+    }
+  }
+
+
+  .utility_old_mg {
+    margin-top: 38px;
+  }
+
+  .utility_old_align {
+    text-align: center;
   }
 
   .container {
-  }
+
+  } */ 
 </style>
