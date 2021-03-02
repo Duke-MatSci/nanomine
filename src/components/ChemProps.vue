171aae5d3f18b8a6b338ee6f88813d726165fe25
tolulomo <tolulomofateye@gmail.com>
2021-03-02 12:19:18 -0500

    chemprops update

--- src/components/ChemProps.vue
+++ src/components/ChemProps.vue
@@ -58,7 +58,7 @@
               <v-flex xs12 sm6 md6 class="text-xs-left" v-if="stdname !== ''">
                 <p class="text-xs-left">Standardized chemical name and density information:</p>
                 <v-text-field v-model="stdname" label='Standardized Name' outlined></v-text-field>
-                <v-text-field v-model="density" label='Density (g/cm3)' outlined></v-text-field>
+                <v-text-field v-model="density" label='Density (g/cm3 at 25°C)' outlined></v-text-field>
                 <v-text-field v-model="uSMILES" label='uSMILES' outlined v-if="pfRadios === 'pol'"></v-text-field>
                 <p v-if="pfRadios === 'pol'">Structure
                 <Smiles :smilesOptions="smilesOptions" :smilesInput="inputStr" :formulaHandler="formulaUpdated" :onSuccessHandler="onSuccess" :onErrorHandler="onError" height="100%" width="100%"></Smiles>
