60e0abe4ed29052b30a2fa43afdb3f3d0cb2f1ea
tolulomo <tolulomofateye@gmail.com>
2021-04-26 17:33:22 -0400

    menu update

--- src/components/utils/header.vue
+++ src/components/utils/header.vue
@@ -78,7 +78,7 @@
                                     <span class="nav_menu--siblings-lists" @click="links('/nm#/tools')"><a href="/nm#/tools">Module Tools</a></span>
                                     <span class="nav_menu--siblings-lists" @click="links('/nm#/simmtools')"><a href="/nm#/simmtools">Simulation Tools</a></span>
                                     <span class="nav_menu--siblings-lists" @click="links('/nm#/ChemProps')"><a href="/nm#/ChemProps">ChemProps</a></span>
-                                    <span class="nav_menu--siblings-lists" @click="links('/nm#/CurationPlot')"><a href="/nm#/CurationPlot">Easy csv Plotter</a></span>
+                                    <!--<span class="nav_menu--siblings-lists" @click="links('/nm#/CurationPlot')"><a href="/nm#/CurationPlot">Easy csv Plotter</a></span>-->
                                     <!--<span class="nav_menu--siblings-lists" @click="links('/nm#/pixelunit')"><a href="/nm#/pixelunit">Geometry Explorer</a></span>-->
                                 </div>
                             </div>
